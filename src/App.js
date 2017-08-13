f4401274ccfe68f444a043830493cf7e0e34d054
Dionne Phua <li.ann86@gmail.com>
2017-08-14 01:40:04 +0800

    Made changes to nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Grid} from 'react-bootstrap';
+import {Grid, Navbar, Nav} from 'react-bootstrap';
 
 import {HashRouter, Link} from 'react-router-dom';
 import Main from './Routes';
@@ -7,22 +7,24 @@ import Main from './Routes';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
         {/* side nav bar */}
-        <div className="area"></div>
+        {/* <div className="area"></div>
           <nav className="main-menu">
 
           <ul>
               <li>
-                  <a href="/" className="nav-a">
-
+                  <a className="nav-a">
+                      <Link to="/" className="nav-a-Link">
                       <i className="fa fa-home fa-2x nav-fa"></i>
                       <span className="nav-text">
                           Home
                       </span>
+                      </Link>
                   </a>
 
               </li>
@@ -82,9 +84,70 @@ class App extends Component {
 
               </li>
             </ul>
-      </nav>
+      </nav> */}
 
+      <section>
+       <article>
+         <div className="menu">
+           <input id="menu--toggle" type="checkbox" />
+           {/* /#menu--toggle */}
+           <label className="menu--toggle__trigger" htmlFor="menu--toggle" />
+           {/* /.menu--toggle__trigger */}
+           <label className="menu--toggle__burger" htmlFor="menu--toggle" />
+           {/* /.menu--toggle__burger */}
+           <ul className="menu__body">
+             <li className="menu__body-element">
+              <Link to="/" className="nav-a-Link">
+               <a className="menu__body-link" >
+                 Home
+               </a>
+              </Link>
+               {/* /.menu__body-link */}
+             </li>
+             {/* /.menu__body-element */}
+             <li className="menu__body-element">
+               <a className="menu__body-link" href="https://dionnephualiann.github.io/game_project/" target="_blank">
+                 Burger Stack em' Up
+               </a>
+               {/* /.menu__body-link */}
+             </li>
+             {/* /.menu__body-element */}
+             <li className="menu__body-element">
+               <a className="menu__body-link" href="https://project2-summon-auntie.herokuapp.com/" target="_blank">
+                 Report Summon Auntie
+               </a>
+               {/* /.menu__body-link */}
+             </li>
 
+             <li className="menu__body-element">
+               <a className="menu__body-link" href="https://ancient-everglades-11615.herokuapp.com/" target="_blank">
+                 Mentor Match
+               </a>
+               {/* /.menu__body-link */}
+             </li>
+
+             <li className="menu__body-element">
+               <a className="menu__body-link" href="https://expressive-86f54.firebaseapp.com/" target="_blank">
+                 Expressive
+               </a>
+               {/* /.menu__body-link */}
+             </li>
+
+             <li className="menu__body-element">
+              <Link to="/contact" className="nav-a-Link">
+               <a className="menu__body-link">
+                 Contact Me
+               </a>
+              </Link>
+               {/* /.menu__body-link */}
+             </li>
+             {/* /.menu__body-element */}
+           </ul>
+           {/* /.menu__body */}
+         </div>
+         {/* /.menu */}
+       </article>
+     </section>
 
       <Grid>
         <Main>
--- src/components/LandingPage/LandingHeader/LandingHeader.js
+++ src/components/LandingPage/LandingHeader/LandingHeader.js
@@ -13,7 +13,7 @@ class LandingHeader extends Component {
            <Col md={4} className="header-contents pull-left">
              <div className="header-title">Dionne Phua</div>
              <p className="subheader-text">Web Developer | Concept Artist</p>
-             <a href="#about" className="btn btn-primary btn-xl page-scroll">
+             <a href="#about" className="btn btn-primary btn-xl page-scroll" id="page-scroll">
              Find Out More!</a>
            </Col>
            </Col>
